38be77b60c0f387ccb79e6a5e9650b448b719d1d
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-06-26 18:25:49 +0200

    remove test login credentials

--- src/commands/LoginCommand.ts
+++ src/commands/LoginCommand.ts
@@ -12,19 +12,19 @@ async function loginCommand(context: vscode.ExtensionContext, client: Client, on
 
     const fusioUrl = await vscode.window.showInputBox({
         title: 'Url',
-        value: 'http://127.0.0.1/projects/fusio/public/index.php/',
+        value: '',
         placeHolder: 'Endpoint url of your Fusion instance i.e. https://demo.fusio-project.org/'
     });
 
     const username = await vscode.window.showInputBox({
         title: 'Username',
-        value: 'test',
+        value: '',
         placeHolder: 'Your username'
     });
     
     const password = await vscode.window.showInputBox({
         title: 'Password',
-        value: 'test1234',
+        value: '',
         placeHolder: 'Your password',
         password: true
     });
